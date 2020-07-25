ed8e4eeebe34e8d1141461545ac00b2e8d973e18
Sukarno <sukarnooke@gmail.com>
2020-07-25 10:51:48 +0700

    Add dashboard page

---
+++ static/js/dashboard.js
@@ -0,0 +1,42 @@
+new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data: {
+      dialog: false,
+      drawer: null,
+      items: [
+        {heading: 'TITLE'},
+        { icon: 'mdi-contacts', text: 'Contacts' },
+        { icon: 'mdi-history', text: 'Frequently contacted' },
+        { icon: 'mdi-content-copy', text: 'Duplicates' },
+        {
+          icon: 'mdi-chevron-up',
+          'icon-alt': 'mdi-chevron-down',
+          text: 'Labels',
+          model: true,
+          children: [
+            { icon: 'mdi-plus', text: 'Create label' },
+          ],
+        },
+        {
+          icon: 'mdi-chevron-up',
+          'icon-alt': 'mdi-chevron-down',
+          text: 'More',
+          model: false,
+          children: [
+            { text: 'Import' },
+            { text: 'Export' },
+            { text: 'Print' },
+            { text: 'Undo changes' },
+            { text: 'Other contacts' },
+          ],
+        },
+        { icon: 'mdi-cog', text: 'Settings' },
+        { icon: 'mdi-message', text: 'Send feedback' },
+        { divider: true},
+        { icon: 'mdi-help-circle', text: 'Help' },
+        { icon: 'mdi-cellphone-link', text: 'App downloads' },
+        { icon: 'mdi-keyboard', text: 'Go to the old version' },
+      ],
+    },
+});
